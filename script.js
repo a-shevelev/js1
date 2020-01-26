8bae16c4f88d3860a7a8469e36ec964442d1bb0d
a-shevelev <60062720+a-shevelev@users.noreply.github.com>
2020-01-26 14:03:26 +0300

    punkt2.1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("all ok")
+punkt2_1()
+
+function punkt2_1() {
+  let d = {key: "value"};
+  let e = {key: "value"} == d;
+  console.log(e);
+  let a = {key: "value"} === d;
+  console.log(a);
+  let b = d;
+  let c = b ===d;
+  console.log(c);
+}
